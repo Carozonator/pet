edce2e598d126cef18abb64ae6427fb18a9f50e4
Rafael Arcieri <rafa@rafaels-air.home>
2014-07-27 19:10:41 -0400

    quick css

--- application/module/anuncio/anunciolist.php
+++ application/module/anuncio/anunciolist.php
@@ -9,7 +9,7 @@ foreach($data as $row){
     <div style="margin:40px;position:relative;"class="img150">
         <div style="float:left;margin-right:10px;" class="thumb">
             <a title="<?php echo $row['titulo'];?>" href="/anuncio/<?php echo $row['id'];?>">
-                <img alt="<?php echo $row['nombre_original'];?>" src="<?php echo MEDIA.'upload/'.$row['foto_1']; ?>">
+                <img style="height:100%;" alt="<?php echo $row['nombre_original'];?>" src="<?php echo MEDIA.'upload/'.$row['foto_1']; ?>">
             </a>
         </div>
         <div class="overflow mbottom">

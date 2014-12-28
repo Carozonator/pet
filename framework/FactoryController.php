03782a1fc711fcd43d04f04705f4a88a7678d706
System Administrator <root@Rafaels-MacBook-Air.local>
2014-12-28 10:16:17 -0500

    aaa

--- framework/FactoryController.php
+++ framework/FactoryController.php
@@ -10,9 +10,7 @@ class FactoryController{
         $view = self::buildView($request);
         
         $controller_name = __NAMESPACE__.'\\'.$controller_name;
-        echo ROOT.'application/module/'.strtolower($request->getController()).'/'.ucfirst($request->getController()).'Controller.php';
         require_once ROOT.'application/module/'.strtolower($request->getController()).'/'.ucfirst($request->getController()).'Controller.php';
-        echo 'after error';
         /*
         if(! class_exists($controller_name)){
             $error = FactoryController::build('ErrorController');

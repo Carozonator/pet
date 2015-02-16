093bed248a0bef680e63e35b55a3532011f8e61d
System Administrator <root@rafaels-air.home>
2015-02-15 20:44:46 -0500

    prdoucto error

--- application/module/producto/productoController.php
+++ application/module/producto/productoController.php
@@ -8,10 +8,10 @@ class ProductoController extends Controller{
         $producto = new \pluralpet\Producto();
         $result = $producto->get($this->request->getMethod());
         
-        $foto = new \pluralpet\Foto();
-        $fotos = $foto->get($this->request->getMethod(),'producto');
+        //$foto = new \pluralpet\Foto();
+        //$fotos = $foto->get($this->request->getMethod(),'producto');
         
-        $this->view->assign(array('foto'=>$fotos));
+        //$this->view->assign(array('foto'=>$fotos));
         $this->view->assign(array('producto'=>$result));
         $this->view->setFile('producto');
         $this->view->render();

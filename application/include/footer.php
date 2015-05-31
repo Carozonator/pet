d289a7022cb038874ef9bad0a7f1aeaeca2b5db2
Rafael Arcieri <rafa@rafaels-air.home>
2015-05-31 14:29:35 -0400

    fotos account

--- application/include/footer.php
+++ application/include/footer.php
@@ -60,7 +60,7 @@
                     </div>                
                 </nav><!-- .f_menu -->
             </div><!-- .grid_3 -->
-            <div class="grid_3" style="margin-top:-42px;">
+            <div class="grid_3 facebook_footer" style="margin-top:-41px;">
                 
                 <div class="fb-like-box" data-width="235" data-href="https://www.facebook.com/pages/PluralPet/741155449229264?ref=hl" data-colorscheme="dark" data-show-faces="true" data-header="false" data-stream="false" data-show-border="false"></div>
                 

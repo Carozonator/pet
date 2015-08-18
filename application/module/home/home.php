41b175f7ba3c7aca79b52521a8cca15b449e25cb
Rafael Arcieri <rafa@rafaels-air.home>
2015-08-17 21:48:52 -0400

    cerrar secion

--- application/module/home/home.php
+++ application/module/home/home.php
@@ -1,8 +1,5 @@
 <?php 
 
-//print_r($servicio);die;
-
-
 $carrusel = array(/*array('title'=>'','text'=>'Encuentrala de manera sensilla','loc'=>'top:20px;left:600px;','img'=>'comprar.jpg'),*/
                      array('title'=>'','text'=>'Conoce los mejores consejos para disfrutar al máximo de tu mascota.','loc'=>'left:0px;','img'=>'consejos.jpg','link'=>'/consejo/perro/'),
                  array('title'=>'','text'=>'Abasto','loc'=>'left:0px;','img'=>'abasto.jpg','link'=>'https://www.facebook.com/abasto.deraciones'),
@@ -16,11 +13,7 @@ $carrusel = array(/*array('title'=>'','text'=>'Encuentrala de manera sensilla','
                  array('title'=>'','text'=>'Solvit','loc'=>'left:0px;','img'=>'solvit.jpg','link'=>'http://www.solvitproducts.com/'),
                      array('title'=>'','text'=>'Agrega un nuevo integrante a tu familia.','loc'=>'right:0px;','img'=>'comprar.jpg','link'=>'/comprar/perro/'),
                  array('title'=>'','text'=>'Reyes de la Pradera','loc'=>'left:0px;','img'=>'reyes.jpg','link'=>'https://www.facebook.com/reyesdela.pradera?ref=br_rs'),
-                 
-    
     );
-//echo '<pre>';
-//print_r($perro[0]);die;
 ?>
 
 
@@ -60,7 +53,7 @@ $carrusel = array(/*array('title'=>'','text'=>'Encuentrala de manera sensilla','
     });
 </script>
 <div id="slider1_container">
-    <div u="slides" style="cursor: move; position: absolute; overflow: hidden; left: 0px; top: 0px; width: 984px; height: 480px;" class="slider_inner">
+    <div u="slides" class="slider_inner">
         <?php 
         foreach($carrusel as $key => $row){
             $src = MEDIA.'carrusel/'.$row['img'];
@@ -70,21 +63,16 @@ $carrusel = array(/*array('title'=>'','text'=>'Encuentrala de manera sensilla','
             echo '</div>';
         }
         ?>
-        <div u="thumbnavigator" class="sliderb-T" style="position: absolute; bottom: 0px; left: 0px; height:45px; width:984px;">
-            <div style="filter: alpha(opacity=40); opacity:0.4; position: absolute; display: block;
-                background-color: #000000; top: 0px; left: 0px; width: 100%; height: 100%;">
+        <div u="thumbnavigator" class="sliderb-T">
+            <div class="sliderb-T_inner">
             </div>
             
             <div u="slides">
-                <div u="prototype" style="POSITION: absolute; WIDTH: 984px; HEIGHT: 45px; TOP: 0; LEFT: 0;">
-                    <div u="thumbnailtemplate" style="font-family: verdana; font-weight: normal; POSITION: absolute; WIDTH: 100%; HEIGHT: 100%; TOP: 0; LEFT: 0; color:#fff; line-height: 45px; font-size:20px; padding-left:10px;"></div>
+                <div u="prototype" class="slider_prototype">
+                    <div u="thumbnailtemplate" class="slider_prototype_thumbnail"></div>
                 </div>
             </div>
         </div>
-        
-        
-        
-        
         <style>
             .jssora14l, .jssora14r, .jssora14ldn, .jssora14rdn
             {
@@ -106,67 +94,12 @@ $carrusel = array(/*array('title'=>'','text'=>'Encuentrala de manera sensilla','
         <span u="arrowright" class="jssora14r" style="width: 30px; height: 50px; top: 123px; right: 0px">
         </span>
         <a style="display: none" href="http://www.jssor.com">Image Slider</a>
-        
-        
-        
-        
-        
-        
-        
     </div>
 </div>
-            <!--
-            <div class="slidprev" style="display: block;margin-top:230px;margin-left:20px;"><span>Prev</span></div>
-            <div class="slidnext" style="display: block;margin-top:230px;margin-right:24px;"><span>Next</span></div>
-            <div class="caroufredsel_wrapper" style="display: block; text-align: start; float: none; position: relative; top: auto; right: auto; bottom: auto; left: 20px; z-index: auto; width: 940px; height: 500px; margin: 0px; overflow: hidden; cursor: move;">
-                <div id="slider" style="text-align: left; float: none; position: absolute; top: 0px; right: auto; bottom: auto; left: 0px; margin: 0px; width: 6888px; height: 500px; z-index: auto;">
-                    
-                        <?php 
-                        $counter=0;
-                        foreach($carrusel as $key => $row){
-                            $src = MEDIA.'carrusel/'.$row['img'];
-                            echo '<div class="slide" style="width: 940px; height: 500px;">';
-                                echo '<img src="'.$src.'" class="attachment-full wp-post-image" alt="slide2">';
-                                echo '<div class="slid_text" style="'.$row['loc'].'">';
-                                    if(!empty($row['title'])){
-                                        echo '<h3 class="slid_title"><span>'.$row['title'].'</span></h3>';
-                                    }
-                                    echo '<div><span>'.$row['text'].'</span</div>';
-                                    //echo '<p><span>al m&aacute;ximo de tu mascota</span></p>';
-                                echo '</div>';
-                             echo '</div>';
-                             echo '</div>';
-                        }
-                        ?>
-                        
-                    
-                    
-                </div>
-            </div>
-            -->
             <div class="clear"></div>
-            <div id="myController" class="" style="display: block;">
-                <?php 
-                foreach($carrusel as $key => $row){
-                    //echo '<a href="#" class=""><span>1</span></a>';
-                }
-                ?>
-                <!--<a href="#" class=""><span>1</span></a><a href="#" class="selected"><span>2</span></a><a href="#" class=""><span>3</span></a>-->
-            </div>
+            
         </div><!-- .grid_12 -->
         <div class="clear"></div>
-        <!--
-        <div id="top_button">
-            <div class="grid_4">
-                <p><br>
-                    <a href="#"><br>
-                        <img class="alignnone size-full" alt="Banner " src="http://wordpress.breeze.itembridge.com/wp-content/uploads/2013/03/banner3.png" width="312" height="101"><br>
-                    </a>
-                </p>
-            </div>
-            <div class="clear"></div>
-        </div>
-        -->
         <div class="main-content-box" style="" id="">
             
             <?php 
